f93916a834bb48159cb8327d25853182f398969d
Parthiv Ganguly <parthiv2048@gmail.com>
2026-02-23 16:00:33 -0500

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Parthiv Ganguly on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
